2c55b129fc6ae0a655616769df54185d70fb2507
ArthurSpencer <spencera@dulwich.org.uk>
2018-11-23 11:42:19 +0000

    Update bubblesortoptimized.py

--- bubblesortoptimized.py
+++ bubblesortoptimized.py
@@ -4,7 +4,7 @@ def bubblesort(nums):
         if x == True:
             x = False
             for j in range(0,len(nums) - 1): 
-                for i in range ((0,len(nums) - 2 - j):
+                for i in range ((0,len(nums) - 2 - j)):
                     if nums[i] > nums[i + 1]:
                         nums[i], nums[i + 1] = nums[i + 1], nums[i]
                         x = True
