94f6f00b4e8b5f17fbe68b982d1b2682fcb5903b
ArthurSpencer <spencera@dulwich.org.uk>
2019-01-16 09:22:14 +0000

    Update recbin.py

--- 3.0/recbin.py
+++ 3.0/recbin.py
@@ -1,18 +1,23 @@
 def recbin(li, find, first, last):
     
     mid = int((first + last)/2)
-    print(mid)
-    print(li[mid])
+    #print(mid)
+    #print(li[mid])
     if find > li[mid]:
         first = mid
         recbin(li, find, first, last)
-    if find < li[mid]:
+    elif find < li[mid]:
         last = mid
         recbin(li, find, first, last)
-    if find == li[mid]:
-        return li[mid]
+    elif find == li[mid]:
+        print (li[mid])
+        print ("at")
+        print (mid)
+        
     else:
+        print ("not present")
         return -1
+        
     
 li = [1,2,3,4,5,6,7,8]
 find = 7
