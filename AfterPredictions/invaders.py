9ba44d4de45943c40ab34e15d2936e821639ada1
ArthurSpencer <spencera@dulwich.org.uk>
2019-06-14 12:04:51 +0100

    Update invaders.py

--- AfterPredictions/invaders.py
+++ AfterPredictions/invaders.py
@@ -24,7 +24,36 @@ pygame.display.set_caption("Snow")
 
 ## -- Classes
 ## -- Define the class snow which is a sprite
-class snow(pygame.sprite.Sprite):
+
+class Player(pygame.sprite.Sprite):
+    # Define the constructor for snow
+    def __init__(self, color, width, height):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = 300
+        self.rect.y = size[0] - 480 
+        self.speed = 0
+    #End Procedure
+    # Class update function - runs for each pass through the game loop
+
+    def update(self):
+        
+        if self.rect.y > 480:
+            self.rect.y = 0
+        else:
+            self.rect.y = self.rect.y + self.speed
+
+    def player_set_speed(val):
+        self.speed = val
+
+
+
+class Invader(pygame.sprite.Sprite):
     # Define the constructor for snow
     def __init__(self, color, width, height, speed):
         # Call the sprite constructor
@@ -35,7 +64,7 @@ class snow(pygame.sprite.Sprite):
         # Set the position of the sprite
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, 600)
-        self.rect.y = random.randrange(0, 400)
+        self.rect.y = random.randrange(0, -50)
         self.speed = speed
     #End Procedure
     # Class update function - runs for each pass through the game loop
@@ -55,17 +84,19 @@ class snow(pygame.sprite.Sprite):
 done = False
 # Create a list of the snow blocks
 
-snow_list = pygame.sprite.Group()
+invader_group = pygame.sprite.Group()
 # Create a list of all sprites
 all_sprites_list = pygame.sprite.Group()
 
 # Create the snowflakes
-number_of_flakes = 50 # we are creating 50 snowflakes
+number_of_flakes = 10 # we are creating 50 snowflakes
 for x in range(number_of_flakes):
-    my_snow = snow(WHITE, 5, 5, 1) # snowflakes are white with size 5 by 5 px
-    snow_list.add(my_snow) # adds the new snowflake to the group of snowflakes
-    all_sprites_list.add(my_snow) # adds it to the group of all Sprites
+    my_invader = Invader(WHITE, 10, 10, 1) # snowflakes are white with size 5 by 5 px
+    invader_group.add(my_invader) # adds the new snowflake to the group of snowflakes
+    all_sprites_list.add(my_invader) # adds it to the group of all Sprites
 #Next x
+
+my_player = Player(YELLOW, 10, 10)
 
 # - - Manages how fast screen refreshes
 clock = pygame.time.Clock()
@@ -75,8 +106,17 @@ clock = pygame.time.Clock()
 while not done:
     # - - User input and cont
 
-
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN: # - a key is down
+            if event.key == pygame.K_LEFT: # - if the left key pressed
+                player.player_set_speed(-3) # speed set to -3
+            elif event.key == pygame.K_RIGHT: # - if the right key pressed
+                player.player_set_speed(3) # speed set to 3
+        elif event.type == pygame.KEYUP: # - a key released
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player.player_set_speed(0) # speed set to 0
 
 
 
@@ -90,6 +130,9 @@ while not done:
         #End If
     #Next event
     # - - Game logic goes after this comment
+    # Game logic goes in here
+    # -- when invader hits the player add 5 to score.
+    player_hit_group = pygame.sprite.spritecollide(Player, invader_group, True)
     all_sprites_list.update()
     # - - Screen background is BLACK
     screen.fill (BLACK)
