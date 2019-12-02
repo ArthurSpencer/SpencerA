5a851a18b50a396baddfc2b8f804d62e9d61374c
Arthur <spenceraspencer1@gmail.com>
2019-12-02 19:47:33 +0000

    a

--- epp.py
+++ epp.py
@@ -26,15 +26,66 @@ node.append(-1)
 listofnodes.append(node)
 currentnode = 0
 
+# u = x
+# i = y
+
 found = False
 while found == False:
     # Find new nodes
     # First find position of - in currentstate
     for i in range(3):
-        for u in range(3):   
+        for u in range(3):
             if currentstate[i][u] == '-':
                 dashposition = [i,u]
-    print(dashposition)
+    # print(dashposition)
+
+    # To the left
+    try:
+        check = dashposition[1] - 1
+        if check < 0:
+            x = 5/0
+        print(currentstate[dashposition[0]][check])
+
+        editedstate = currentstate
+        editedstate[dashposition[0]][check] = '-'
+        editedstate[dashposition[0]][dashposition[1]] = currentstate[dashposition[0]][check]
+        print(editedstate)
+        
+    except:
+        pass
+    
+    #To the right
+    try:
+        check = dashposition[1] + 1
+        if check > 2:
+            x = 5/0
+        #print(currentstate[dashposition[0]][check])
+        editedstate = currentstate
+        editedstate[dashposition[0]][check] = '-'
+        editedstate[dashposition[0]][dashposition[1]] = currentstate[dashposition[0]][check]
+        print(editedstate)
+    except:
+        pass
+    
+    # To the top
+    try:
+        check = dashposition[0] - 1
+        if check < 0:
+            x = 5/0
+        #print(currentstate[check][dashposition[1]])
+    except:
+        pass
+    
+    #To the bottom
+    try:
+        check = dashposition[0] + 1
+        if check > 2:
+            x = 5/0
+        #print(currentstate[check][dashposition[1]])
+    except:
+        pass
+
+    
     
     
 
